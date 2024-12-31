b6aeebbd300e91065ce1f59d703c55e4e2726042
KabirG7 <gautamkabir18@hotmail.com>
2024-12-31 19:21:22 +0530

    final commit

--- api/data.js
+++ api/data.js
@@ -1,21 +1,27 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb+srv://MoviesUser:passwordMOV@apikeys.dmrv56o.mongodb.net/taskManagerTasks";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      await mongoose.connect(MONGODB_URI);
-      
-      const Data = mongoose.models.Data || 
-        mongoose.model('Data', new mongoose.Schema({
-          text: String
-        }));
+  try {
+    await mongoose.connect(MONGODB_URI);
+    
+    const Data = mongoose.models.Data || 
+      mongoose.model('Data', new mongoose.Schema({
+        text: String
+      }));
 
+    if (req.method === 'POST') {
       const newData = await Data.create({ text: req.body.text });
-      res.status(200).json(newData);
-    } catch (error) {
-      res.status(500).json({ error: 'Error saving data' });
+      return res.status(200).json(newData);
+    } 
+    
+    if (req.method === 'GET') {
+      const allData = await Data.find();
+      return res.status(200).json(allData);
     }
+    
+  } catch (error) {
+    return res.status(500).json({ error: 'Error processing request' });
   }
 }
